58b8f4a33658f812f96440f91ec420b532c8956d
Andrew Ross <aross9913@gmail.com>
2023-06-14 23:10:57 +0000

    update format

--- main.js
+++ main.js
@@ -3,52 +3,47 @@ currentWeatherData = []; // this is our definition of state.
 
 function fetchData() // our render function
 {
-//send the api key and user input to the api
+  //send the api key and user input to the api
 
-//Axios: where do we get the Data? What do we send to the API? How to bypass any potential costs? 
-// how to defensively code to bypass unnecessarily costs?
+  //Axios: where do we get the Data? What do we send to the API? How to bypass any potential costs? 
+  // how to defensively code to bypass unnecessarily costs?
+  
 }
-function displayTemperature(num)
-{
-
+function displayTemperature(num) {
+// get temperature data from openweather api via axios
 }
-function setState()
-{
-//how do we set the state of based on the data received from the API?
+function setState() {
+  //how do we set the state of based on the data received from the API?
   currentWeatherData = // axios.(something) //saves the data from axios
   
       
 }
-function checkUserInput() 
-{
+function checkUserInput() {
   // validate the user input to see if correct
   // what happens when data is good? thats why we have the check user input function
-  let zip = document.getElementbyId("input") // !!!check the id of the input to make sure it is correct
+  let zip = document.getElementbyId("input").value // !!!check the id of the input to make sure it is correct
   // make sure the textfield provides the input
   const regex = /[!@#$%^&*(),.?":{}|<>]/; // using the regular expression to toss out any and all special characters in the input
+  if (regex.test(input)) {
+    throw new Error('Input contains special characters');
+  }
   try {     //input length is 5 digits, no characters, no special characters
-    if (zip.length > 5) {
+    if (zip.length !== 5) {
       throw new Error('Input has more than 5 integers');
     }
+    fetchData();
+    // what to do when we have good user input
+  RenderUI();
     return false; // No error
   } catch (error) {
     return true; // Error occurred
+    
   }
-    if (regex.test(input)) {
-    throw new Error('Input contains special characters');
-    }
-      fetchData();
-    // what to do when we have good user input
-
-catch{
-//what happens when not good data?   
-
-}
-  RenderUI();
+ 
 }
 RenderUI();
 {
-   setState();
+  setState();
   displayTemperature();
- //sees the data from API; updates state and invokes the setState 
+  //sees the data from API; updates state and invokes the setState 
 }
