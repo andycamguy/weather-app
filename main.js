3184b3108d88be9d43d123d553d680e35abb506c
Andrew Ross <aross9913@gmail.com>
2023-06-15 19:51:29 +0000

    experimenting

--- main.js
+++ main.js
@@ -1,65 +1,51 @@
-const axios = require('axios');
+
+
 const API_Key = "80c0e2a64d9180528d20e2f53ad95081";
 const API_URL = "https://api.openweathermap.org";
-let API_PATH = "/data/2.5/weather";
-const ZIP_CODE = [40390];
-currentWeatherData = null; // this is our definition of state.
+const API_PATH = "/data/2.5/weather";
+const ZIP_CODE = "40390";
+
+let currentWeatherData = null;
+
 window.addEventListener("load", init);
 
 async function init() {
   console.log('hello world');
-   currentWeatherData = getWeatherData(ZIP_CODE);
-}
-async function getWeatherData(zipcode) // our render function
-{
-  //send the api key and user input to the api
   try {
-    // trap any errors here on the backend, 500 400 is logic related
-    //200 successful call
-    // create the options object which is our parameters for the API call
-    let options =
-    {
+    currentWeatherData = await getWeatherData(ZIP_CODE);
+    console.log(currentWeatherData);
+    // RenderUI(currentWeatherData); // Call your render function here
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function getWeatherData(zipcode) {
+  try {
+    const options = {
       params: {
-        baseurl: API_URL,
         zip: zipcode,
         appid: API_Key
       }
-    
-    }
-    axios.get(API_PATH, options);
-  catch (error) {
-
-  }
-  finally
-  {
-
-  }
-}
-//Axios: where do we get the Data? What do we send to the API? How to bypass any potential costs? 
-// how to defensively code to bypass unnecessarily costs?
-function checkUserInput(zip) {
-  // validate the user input to see if correct
-  // what happens when data is good? thats why we have the check user input function
-  let zip = document.getElementbyId("input").value // !!!check the id of the input to make sure it is correct
-  // make sure the textfield provides the input
-  const regex = /[!@#$%^&*(),.?":{}|<>]/; // using the regular expression to toss out any and all special characters in the input
-  if (regex.test(input)) {
-    throw new Error('Input contains special characters');
-  }
-  try {     //input length is 5 digits, no characters, no special characters
-    if (zip.length !== 5) {
-      throw new Error('Input has more than 5 integers');
-    }
-    getWeatherData();
-    // what to do when we have good user input
-    RenderUI();
-    return false; // No error
+    };
+    const response = await axios.get(API_URL + API_PATH, options);
+    return response.data;
   } catch (error) {
-    return true; // Error occurred
-
-  }
-  finally
-  {
-
+    throw new Error('Failed to fetch weather data');
   }
 }
+
+// Additional function to check user input and trigger the weather data retrieval
+function checkUserInput() {
+  console.log('the button is working')
+  const input = document.getElementById("fetch").value;
+  const regex = /^[0-9]{5}$/; // Use a regex to match exactly 5 digits
+
+  if (!regex.test(input)) {
+    throw new Error('Input is invalid');
+  }
+
+  const zipcode = input;
+  getWeatherData(zipcode);
+}
+
